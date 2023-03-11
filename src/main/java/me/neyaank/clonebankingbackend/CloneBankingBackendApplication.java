b98ed57a4317702988b654bbe5aaf6ae8c44fd85
Artem Nersesian <hotmail.noob1109@gmail.com>
2023-03-11 12:16:30 +0100

    Init

---
+++ src/main/java/me/neyaank/clonebankingbackend/CloneBankingBackendApplication.java
@@ -0,0 +1,13 @@
+package me.neyaank.clonebankingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneBankingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloneBankingBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/neyaank/clonebankingbackend/CloneBankingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package me.neyaank.clonebankingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloneBankingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
