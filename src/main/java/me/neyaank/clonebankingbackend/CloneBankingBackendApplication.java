0731c014eec69ae0568824434e1de283895131b6
waterpooint <82314308+waterpooint@users.noreply.github.com>
2023-03-18 20:54:47 +0200

    Hello by Olena

--- src/main/java/me/neyaank/clonebankingbackend/CloneBankingBackendApplication.java
+++ src/main/java/me/neyaank/clonebankingbackend/CloneBankingBackendApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CloneBankingBackendApplication {
     //Hello by Artem Nersesian
+    //Hello by Olena
     public static void main(String[] args) {
         SpringApplication.run(CloneBankingBackendApplication.class, args);
     }
